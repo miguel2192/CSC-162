df9e7f5a2af4db5b0392c30e2ed4bba021936e2d
Miguel A Rodriguez <miguelrdrgz47@gmail.com>
2016-04-10 20:25:43 -0400

    Array

---
+++ Rodriguez_Array/Rodriguez_Array/Rodriguez_Array/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFill = New System.Windows.Forms.Button()
+        Me.bntSearch = New System.Windows.Forms.Button()
+        Me.lblArray = New System.Windows.Forms.Label()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.List1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnFill
+        '
+        Me.btnFill.Font = New System.Drawing.Font("Elephant", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFill.Location = New System.Drawing.Point(105, 92)
+        Me.btnFill.Name = "btnFill"
+        Me.btnFill.Size = New System.Drawing.Size(131, 55)
+        Me.btnFill.TabIndex = 0
+        Me.btnFill.Text = "Fill Array"
+        Me.btnFill.UseVisualStyleBackColor = True
+        '
+        'bntSearch
+        '
+        Me.bntSearch.Font = New System.Drawing.Font("Elephant", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bntSearch.Location = New System.Drawing.Point(105, 168)
+        Me.bntSearch.Name = "bntSearch"
+        Me.bntSearch.Size = New System.Drawing.Size(131, 55)
+        Me.bntSearch.TabIndex = 1
+        Me.bntSearch.Text = "Search"
+        Me.bntSearch.UseVisualStyleBackColor = True
+        '
+        'lblArray
+        '
+        Me.lblArray.AutoSize = True
+        Me.lblArray.Font = New System.Drawing.Font("Broadway", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblArray.Location = New System.Drawing.Point(12, 23)
+        Me.lblArray.Name = "lblArray"
+        Me.lblArray.Size = New System.Drawing.Size(319, 19)
+        Me.lblArray.TabIndex = 2
+        Me.lblArray.Text = "Welcome to your array of size 10"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Font = New System.Drawing.Font("Broadway", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.Location = New System.Drawing.Point(28, 51)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(284, 19)
+        Me.lblInfo.TabIndex = 3
+        Me.lblInfo.Text = "Don't Enter Duplicate Names"
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Elephant", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(278, 343)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(57, 32)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'List1
+        '
+        Me.List1.FormattingEnabled = True
+        Me.List1.Location = New System.Drawing.Point(105, 245)
+        Me.List1.Name = "List1"
+        Me.List1.Size = New System.Drawing.Size(152, 121)
+        Me.List1.TabIndex = 5
+        '
+        'frmArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Fuchsia
+        Me.ClientSize = New System.Drawing.Size(347, 387)
+        Me.Controls.Add(Me.List1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.lblArray)
+        Me.Controls.Add(Me.bntSearch)
+        Me.Controls.Add(Me.btnFill)
+        Me.Name = "frmArray"
+        Me.Text = "Array Fill & Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnFill As System.Windows.Forms.Button
+    Friend WithEvents bntSearch As System.Windows.Forms.Button
+    Friend WithEvents lblArray As System.Windows.Forms.Label
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents List1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Rodriguez_Array/Rodriguez_Array/Rodriguez_Array/Form1.vb
@@ -0,0 +1,204 @@
+﻿'Title: Array Fill & Search
+'Author:    Miguel Rodriguez
+'Date:      4/07/2016
+'Course:    CSC 162
+'Section:   001
+'Description:   This program creates an array of size 10.
+'   it then proceeds to ask the user for 10 names and fills
+'   the array with the 10 names. More after it then provides
+'   an option to the user to search the array for an specific 
+'   name. It also provides the option to exit the program by
+'   clicking on a button.
+'
+'Initial Algorithm:
+'   1.  Iniutialize the array of names to 10
+'   2.  ask the user for 10 names
+'   3.  Fills the array with the 10 names
+'   4.  provides the user an option to search the array for a name
+'   5.  provides the user with the option to repeat the program
+'   6.  provides the option to exit the program
+'
+'Data Requirements:
+'   Input:
+'       Enter 10 names
+'       Enter name that you wish to search for
+'   Output:
+'       None
+'   Additional:
+'       None
+'
+'Formulas:
+'
+Public Class frmArray
+
+    'Global Constants
+    Const MAX_SIZE As Integer = 10
+
+
+    'Global variables
+    Dim names(MAX_SIZE) As String     'initialize a fixed array to 10
+    Dim counter As Integer         'holds the counter
+    Dim sentinel As String = "y"   'holds a sentinel value
+    Private Sub btnFill_Click(sender As Object, e As EventArgs) Handles btnFill.Click
+        'Refined Algorithm
+        ' Initialize global and local constants
+        ' Initialize global and local variables
+        ' create a names array an initialize to 10
+        ' initilize counter to 0
+        ' WHILE sentinel equals y
+        '    FOR counter less 9
+        '       Get 10 names from the user
+        '       check for numeric input
+        '       IF name is not a numbers
+        '          store names in names array
+        '       ELSE
+        '          display an error
+        '       END IF
+        '    END FOR
+        '    Do you wish to continue
+        '    IF sentinel equals y
+        '       re-inilize everything
+        '    END IF
+        ' END WHILE
+        ' create a buttom to search for names
+        ' WHILE sentinel equals y
+        '    For counter < 9
+        '       IF searchname is equal to array index
+        '          display name was found in cell #
+        '          increase variable found by one
+        '       END IF
+        '    END FOR
+        '    IF found <= 0
+        '       Display name wasn't found
+        '    END IF
+        '    Do you wish to continue y or n
+        '    IF sentinel equals y
+        '       re-inilize everything
+        '    END IF
+        ' END WHILE
+        ' Allows the user to exit the program by pressing a button
+        ' Tests for empty space input
+
+        'Local Constants
+
+
+        'Local Variables
+        Dim name As String           'holds the user's number
+        Dim index As Integer
+
+        While sentinel = "y" Or sentinel = "Y"
+
+            'enables the main button
+            btnFill.Enabled = True
+
+            'warning message telling the user that their grade is not in range
+            MessageBox.Show("Please enter a name!", "Instructions!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+            
+            'rusn 10 times
+            For counter = 0 To 9
+
+                name = InputBox("Please enter a name: ", "Enter a name!", "") 'prompt's for name
+                If IsNumeric(name) Then  'check for numeric input
+
+                    'warning
+                    MessageBox.Show("Please enter a name NOT a numeric input!", "Invalid Entry!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+                    'decrease counter
+                    counter -= 1
+
+                Else
+
+                    'fills array with name
+                    names(counter) = name
+                End If
+
+            Next counter
+
+            'displays array is full
+            MessageBox.Show("All names have been entered and stored successfully!", "Thank you for your input!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            'disables the main buttom
+            btnFill.Enabled = False
+
+            sentinel = InputBox("Do you want to continue[y or n]?: ", "Enter y or n!", "") 'prompt's for name
+
+            're-initializes everything
+            If sentinel = "y" Or sentinel = "Y" Then
+
+                'enables the main buttom
+                btnFill.Enabled = True
+
+                're-initializes counter
+                counter = 0
+
+            End If
+
+        End While
+        'runs 10 times
+        For index = 0 To 9
+
+            'lists the array elements using a list box
+            List1.Items.Add("Index " & index & " -----" & names(index))
+
+        Next index
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close() 'closes the application
+        End
+    End Sub
+
+    Private Sub bntSearch_Click(sender As Object, e As EventArgs) Handles bntSearch.Click
+
+        'Local Contants
+
+        'Local variables
+        Dim SearchName As String       'holds the name being search
+        Dim found As Integer           'holds how many times was found
+        Dim counter1 As Integer = 0        'holds a value for counter1
+Line1:
+        'sets sentinel to true
+        sentinel = "y"
+
+        'stores the name that is been search for
+        SearchName = InputBox("Enter the name that you wish to search for: ", "Enter a name!", "") 'prompt's for the user's guess
+
+        While sentinel = "y" Or sentinel = "Y"
+            For counter1 = 0 To 9
+
+                'checks if the random num is equal to index
+                If SearchName = names(counter1) Then
+
+                    'Displays name was found in cell "#" 
+                    MessageBox.Show(SearchName & " was found in cell " & counter1, "Thank you for your input!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                    'increase found by one
+                    found += 1
+
+                End If
+
+            Next counter1
+
+            'if name is not found
+            If found <= 0 Then
+                MessageBox.Show("We search the array for " & SearchName & " but we cound't find it!", "Thank you for your input!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+
+            'asks the user if they want to repeat
+            sentinel = InputBox("Do you want to continue[y or n]?: ", "Enter y or n!", "") 'prompt's for name
+
+            If sentinel = "y" Or sentinel = "Y" Then
+
+                're-initializes found 
+                found = 0
+
+                're-initilizes counter1
+                counter1 = 0
+                GoTo Line1
+
+            End If
+        End While
+    End Sub
+End Class
