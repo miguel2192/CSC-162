dbb6b5ab0a876c2b2b364f746e76f997c77ce425
Miguel A Rodriguez <miguelrdrgz47@gmail.com>
2016-04-17 20:43:57 -0400

    2D array

---
+++ Rodriguez_Lab2DArray/Rodriguez_Lab2DArray/Rodriguez_Lab2DArray/2dArray.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm2dArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnCMD = New System.Windows.Forms.Button()
+        Me.List1 = New System.Windows.Forms.ListBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.lblTitle.Font = New System.Drawing.Font("Elephant", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.lblTitle.Location = New System.Drawing.Point(135, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(302, 35)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Voting Information "
+        '
+        'btnCMD
+        '
+        Me.btnCMD.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnCMD.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCMD.Location = New System.Drawing.Point(185, 64)
+        Me.btnCMD.Name = "btnCMD"
+        Me.btnCMD.Size = New System.Drawing.Size(196, 50)
+        Me.btnCMD.TabIndex = 1
+        Me.btnCMD.Text = "Enter Votes"
+        Me.btnCMD.UseVisualStyleBackColor = False
+        '
+        'List1
+        '
+        Me.List1.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.List1.FormattingEnabled = True
+        Me.List1.ItemHeight = 16
+        Me.List1.Location = New System.Drawing.Point(12, 176)
+        Me.List1.Name = "List1"
+        Me.List1.Size = New System.Drawing.Size(544, 164)
+        Me.List1.TabIndex = 2
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnDisplay.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplay.Location = New System.Drawing.Point(185, 120)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(196, 50)
+        Me.btnDisplay.TabIndex = 3
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnExit.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(485, 346)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(71, 34)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frm2dArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(568, 394)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.List1)
+        Me.Controls.Add(Me.btnCMD)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frm2dArray"
+        Me.Text = " 2D Array"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents btnCMD As System.Windows.Forms.Button
+    Friend WithEvents List1 As System.Windows.Forms.ListBox
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Rodriguez_Lab2DArray/Rodriguez_Lab2DArray/Rodriguez_Lab2DArray/2dArray.vb
@@ -0,0 +1,118 @@
+﻿
+'Title: Lab 2D Array
+'Author:    Miguel Rodriguez
+'Date:      4/07/2016
+'Course:    CSC 162
+'Section:   001
+'Description:   This program creates a 2d array. It them lets the user 
+'   store the number of votes for four diferrent cities and three 
+'   different voting groups in the United States. It then proceeds
+'   and prints out everything in a table fashion to the user
+'
+'Initial Algorithm:
+'   1.  Initialize a voter group array to 3
+'   2.  Initialize a city array to 4
+'   3.  Iniutialize a 2d array to size (4,3)
+'   4.  ask the user for the number of votes for each city
+'   5.  Fills the 2 dimentional array with user input
+'   6.  Prints the array to the user in a table fashion
+'   7.  provides the user with the option to repeat the program
+'   8.  provides the option to exit the program
+'
+'Data Requirements:
+'   Input:
+'       Enter votes
+'   Output:
+'       None
+'   Additional:
+'       None
+'
+'Formulas:
+'
+Public Class frm2dArray
+
+    'global constants
+
+    'global variables
+    Dim voterGroup(3) As String     'holds the voter groups array
+    Dim CityArray(4) As String      'holds the city array
+    Dim Votes(4, 3) As String       'holds the 2 dimesional array
+
+    Private Sub btnCMD_Click(sender As Object, e As EventArgs) Handles btnCMD.Click
+
+        'local constants
+
+        'local variables
+        Dim row As Integer               'row counter
+        Dim col As Integer               'column counter
+        Dim voteNum As String            'holds the voteNum (not tested)
+        Dim validNumber As Integer       'holds valid votes
+
+
+        voterGroup(1) = "Democrats"      'inserts democrats in index 1 in array voterGroup
+        voterGroup(2) = "Republicans"    'inserts Republicans in index 2 in array voterGroup
+        voterGroup(3) = "Independents"   'inserts Independents in index 3 in array voterGroup
+        CityArray(1) = "East Side"       'inserts East Side in index 1 in array CityArray
+        CityArray(2) = "West Side"       'inserts West Side in index 2 in array CityArray
+        CityArray(3) = "South side"      'inserts South side in index 3 in array CityArray
+        CityArray(4) = "North Side"      'inserts North Side in index 4 in array CityArray
+
+        For row = 1 To 4 'while on a row
+
+            For col = 1 To 3 'cycle through it's columns
+                voteNum = InputBox("Please Enter number of " & voterGroup(col) & " votes for " & CityArray(row), "Area")
+                If IsNumeric(voteNum) Then  'check for numeric input
+                    validNumber = Val(voteNum) 'convert to integer
+                    Votes(row, col) = validNumber 'inserts calid votes into the 2 dimensional array
+                Else
+
+                    'warning message to the user that the input that they entered is a letter not a number
+                    MessageBox.Show("Please enter a numeric value!", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    col -= 1 'decrease counter by one
+
+                End If
+
+            Next col
+        Next row
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+
+        'Local Constants
+
+        'Local variables
+        Dim outputString As String
+        'Print out 2-D Array
+        outputString = String.Format("{0, 15}", " ") 'print 15 spaces before column header is printed
+
+        For col = 1 To 3 'print column header
+
+            'print each Product name on field width of 10 and stay on same line
+            outputString = outputString & String.Format("{0, 15}", voterGroup(col))
+        Next col
+
+        'print outputString in Listbox
+        List1.Items.Add(outputString) 'will automatically go to next line
+
+        For row = 1 To 4 'while on a row
+
+            'print the city on field width of 15 and stay on same line
+            outputString = String.Format("{0, 20}", CityArray(row))
+            For col = 1 To 3 'cycle through it's columns
+                'print product of row x col on field width of 4
+                'stay on same line for each column value
+                outputString = outputString + String.Format("{0, 10}", Votes(row, col))
+            Next col
+            'print outputString in Listbox
+            List1.Items.Add(outputString) 'Move down to next row
+        Next row
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close() 'closes the application
+        End
+    End Sub
+End Class
